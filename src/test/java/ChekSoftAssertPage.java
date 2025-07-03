15315b3903e3e67f06eb83dab95a386b17c158c3
Vadim R. <gorodnihi@yandex.ru>
2025-07-03 18:54:38 +0300

    New hw, started test

---
+++ src/test/java/ChekSoftAssertPage.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ChekSoftAssertPage {
+    @BeforeAll
+    static void setupConfig() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1200x900";
+        Configuration.baseUrl = "https://github.com/";
+    }
+
+    @Test
+    public void checkSoftAssertPage() {
+
+        open("selenide/selenide");
+        $(("#wiki-tab")).click();
+    }
+}
