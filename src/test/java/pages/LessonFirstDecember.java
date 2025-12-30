6b2c7a8eb8e8453197e62be272f0509b419b14e1
Vadim R. <gorodnihi@yandex.ru>
2025-12-30 16:34:14 +0300

    pushed

--- src/test/java/pages/LessonFirstDecember.java
+++ src/test/java/pages/LessonFirstDecember.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
@@ -23,12 +25,19 @@ public class LessonFirstDecember {
         Configuration.browserSize = "1200x900";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 30000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     PObjects mainPage = new PObjects();
     static Faker FakerTest = new Faker();
 
-    @Tag("formtest")
+    @Tag("jenkinstest")
   //  @Disabled
     @DisplayName("Тестируем форму на сайте")
     @MethodSource("emailProvider")
